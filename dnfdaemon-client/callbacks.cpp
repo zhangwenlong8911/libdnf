329162f264b902abdd75e322ec75fb3ffc7a4ea7
Marek Blaha <mblaha@redhat.com>
2021-04-06 17:34:19 +0200

    dnfdaemon-client: Transaction progress bars

--- dnfdaemon-client/callbacks.cpp
+++ dnfdaemon-client/callbacks.cpp
@@ -205,4 +205,199 @@ void PackageDownloadCB::mirror_failure(sdbus::Signal & signal) {
         multi_progress_bar.print();
     }
 }
+
+
+
+TransactionCB::TransactionCB(sdbus::IProxy * proxy, std::string session_object_path)
+    : DbusCallback(proxy, session_object_path) {
+    // register signal handlers
+    proxy->registerSignalHandler(
+        dnfdaemon::INTERFACE_RPM, dnfdaemon::SIGNAL_TRANSACTION_VERIFY_START, [this](sdbus::Signal & signal) -> void {
+            this->verify_start(signal);
+        });
+    proxy->registerSignalHandler(
+        dnfdaemon::INTERFACE_RPM, dnfdaemon::SIGNAL_TRANSACTION_VERIFY_PROGRESS, [this](sdbus::Signal & signal) -> void {
+            this->verify_progress(signal);
+        });
+    proxy->registerSignalHandler(
+        dnfdaemon::INTERFACE_RPM, dnfdaemon::SIGNAL_TRANSACTION_VERIFY_STOP, [this](sdbus::Signal & signal) -> void {
+            this->verify_end(signal);
+        });
+    proxy->registerSignalHandler(
+        dnfdaemon::INTERFACE_RPM, dnfdaemon::SIGNAL_TRANSACTION_TRANSACTION_START, [this](sdbus::Signal & signal) -> void {
+            this->transaction_start(signal);
+        });
+    proxy->registerSignalHandler(
+        dnfdaemon::INTERFACE_RPM, dnfdaemon::SIGNAL_TRANSACTION_TRANSACTION_PROGRESS, [this](sdbus::Signal & signal) -> void {
+            this->transaction_progress(signal);
+        });
+    proxy->registerSignalHandler(
+        dnfdaemon::INTERFACE_RPM, dnfdaemon::SIGNAL_TRANSACTION_TRANSACTION_STOP, [this](sdbus::Signal & signal) -> void {
+            this->transaction_end(signal);
+        });
+    proxy->registerSignalHandler(
+        dnfdaemon::INTERFACE_RPM, dnfdaemon::SIGNAL_TRANSACTION_ACTION_START, [this](sdbus::Signal & signal) -> void {
+            this->action_start(signal);
+        });
+    proxy->registerSignalHandler(
+        dnfdaemon::INTERFACE_RPM, dnfdaemon::SIGNAL_TRANSACTION_ACTION_PROGRESS, [this](sdbus::Signal & signal) -> void {
+            this->action_progress(signal);
+        });
+    proxy->registerSignalHandler(
+        dnfdaemon::INTERFACE_RPM, dnfdaemon::SIGNAL_TRANSACTION_ACTION_STOP, [this](sdbus::Signal & signal) -> void {
+            this->action_end(signal);
+        });
+    proxy->registerSignalHandler(
+        dnfdaemon::INTERFACE_RPM, dnfdaemon::SIGNAL_TRANSACTION_SCRIPT_START, [this](sdbus::Signal & signal) -> void {
+            this->script_start(signal);
+        });
+    proxy->registerSignalHandler(
+        dnfdaemon::INTERFACE_RPM, dnfdaemon::SIGNAL_TRANSACTION_SCRIPT_STOP, [this](sdbus::Signal & signal) -> void {
+            this->script_stop(signal);
+        });
+    proxy->registerSignalHandler(
+        dnfdaemon::INTERFACE_RPM, dnfdaemon::SIGNAL_TRANSACTION_SCRIPT_ERROR, [this](sdbus::Signal & signal) -> void {
+            this->script_error(signal);
+        });
+}
+
+void TransactionCB::new_progress_bar(uint64_t total, const std::string & description) {
+    if (active_progress_bar &&
+        active_progress_bar->get_state() != libdnf::cli::progressbar::ProgressBarState::ERROR) {
+        active_progress_bar->set_state(libdnf::cli::progressbar::ProgressBarState::SUCCESS);
+    }
+    auto progress_bar = new libdnf::cli::progressbar::DownloadProgressBar(static_cast<int64_t>(total), description);
+    multi_progress_bar.add_bar(progress_bar);
+    progress_bar->set_auto_finish(false);
+    progress_bar->start();
+    active_progress_bar = progress_bar;
+}
+
+void TransactionCB::verify_start(sdbus::Signal & signal) {
+    if (signature_valid(signal)) {
+        uint64_t total;
+        signal >> total;
+        new_progress_bar(total, "Verify package files");
+    }
+}
+
+void TransactionCB::verify_progress(sdbus::Signal & signal) {
+    if (signature_valid(signal) && active_progress_bar) {
+        uint64_t amount;
+        signal >> amount;
+
+        active_progress_bar->set_ticks(static_cast<int64_t>(amount));
+        multi_progress_bar.print();
+    }
+}
+
+void TransactionCB::verify_end(sdbus::Signal & signal) {
+    if (signature_valid(signal) && active_progress_bar) {
+        uint64_t total;
+        signal >> total;
+        active_progress_bar->set_ticks(static_cast<int64_t>(total));
+        multi_progress_bar.print();
+    }
+}
+
+void TransactionCB::transaction_start(sdbus::Signal & signal) {
+    if (signature_valid(signal)) {
+        uint64_t total;
+        signal >> total;
+        new_progress_bar(total, "Prepare transaction");
+    }
+}
+
+void TransactionCB::transaction_progress(sdbus::Signal & signal) {
+    if (signature_valid(signal) && active_progress_bar) {
+        uint64_t amount;
+        signal >> amount;
+
+        active_progress_bar->set_ticks(static_cast<int64_t>(amount));
+        multi_progress_bar.print();
+    }
+}
+
+void TransactionCB::transaction_end(sdbus::Signal & signal) {
+    if (signature_valid(signal) && active_progress_bar) {
+        uint64_t total;
+        signal >> total;
+        active_progress_bar->set_ticks(static_cast<int64_t>(total));
+        multi_progress_bar.print();
+    }
+}
+
+void TransactionCB::action_start(sdbus::Signal & signal) {
+    if (signature_valid(signal)) {
+        std::string nevra;
+        int action;
+        uint64_t total;
+        signal >> nevra;
+        signal >> action;
+        signal >> total;
+        //XXX action to message
+        std::string msg=std::to_string(action) + ": ";
+        new_progress_bar(total, msg + nevra);
+        multi_progress_bar.print();
+    }
+}
+
+void TransactionCB::action_progress(sdbus::Signal & signal) {
+    if (signature_valid(signal) && active_progress_bar) {
+        std::string nevra;
+        uint64_t amount;
+        signal >> nevra;
+        signal >> amount;
+
+        active_progress_bar->set_ticks(static_cast<int64_t>(amount));
+        multi_progress_bar.print();
+    }
+}
+
+void TransactionCB::action_end(sdbus::Signal & signal) {
+    if (signature_valid(signal) && active_progress_bar) {
+        [[maybe_unused]] std::string nevra;
+        uint64_t total;
+        signal >> nevra;
+        signal >> total;
+        active_progress_bar->set_ticks(static_cast<int64_t>(total));
+        multi_progress_bar.print();
+    }
+}
+
+void TransactionCB::script_start(sdbus::Signal & signal) {
+    if (signature_valid(signal) && active_progress_bar) {
+        std::string nevra;
+        signal >> nevra;
+        active_progress_bar->add_message(libdnf::cli::progressbar::MessageType::INFO, "Running scriptlet: " + nevra);
+        multi_progress_bar.print();
+    }
+}
+
+void TransactionCB::script_stop(sdbus::Signal & signal) {
+    if (signature_valid(signal) && active_progress_bar) {
+        std::string nevra;
+        signal >> nevra;
+        active_progress_bar->add_message(libdnf::cli::progressbar::MessageType::INFO, "Stop scriptlet: " + nevra);
+        multi_progress_bar.print();
+    }
+}
+
+void TransactionCB::script_error(sdbus::Signal & signal) {
+    if (signature_valid(signal) && active_progress_bar) {
+        std::string nevra;
+        signal >> nevra;
+        active_progress_bar->add_message(libdnf::cli::progressbar::MessageType::ERROR, "Error in scriptlet: " + nevra);
+        multi_progress_bar.print();
+    }
+}
+
+void TransactionCB::unpack_error(sdbus::Signal & signal) {
+    if (signature_valid(signal) && active_progress_bar) {
+        std::string nevra;
+        signal >> nevra;
+        active_progress_bar->add_message(libdnf::cli::progressbar::MessageType::ERROR, "Unpack error: " + nevra);
+        multi_progress_bar.print();
+    }
+}
 }  // namespace dnfdaemon::client
