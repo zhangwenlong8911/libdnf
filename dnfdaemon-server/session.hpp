08d117cbcd7dbd7f7d6b30eaa4c0363f1ac85b53
Marek Blaha <mblaha@redhat.com>
2021-07-28 13:59:37 +0200

    dnfdaemon: Repository key import confirmation
    Implements the server part of repository key import confirmation
    workflow.
    When repository key import is needed, RepoCB::repokey_import() callback
    is called. dnfdaemon-server emits the signal
    SIGNAL_REPO_KEY_IMPORT_REQUEST with the key details, and then waits for
    the clients response.
    Client is supposed to response by calling confirm_key() method of
    repository interface with key id and boolean value indicating whether
    the key should be imported.

--- dnfdaemon-server/session.hpp
+++ dnfdaemon-server/session.hpp
@@ -30,6 +30,7 @@ along with libdnf.  If not, see <https://www.gnu.org/licenses/>.
 #include <sdbus-c++/sdbus-c++.h>
 
 #include <atomic>
+#include <condition_variable>
 #include <memory>
 #include <mutex>
 #include <string>
@@ -81,6 +82,8 @@ public:
     bool check_authorization(const std::string & actionid, const std::string & sender);
     void fill_sack();
     bool read_all_repos();
+    void confirm_key(const std::string & key_id, const bool confirmed);
+    bool wait_for_key_confirmation(const std::string & key_id, sdbus::Signal & signal);
 
 private:
     sdbus::IConnection & connection;
@@ -94,6 +97,11 @@ private:
     std::atomic<dnfdaemon::RepoStatus> repositories_status{dnfdaemon::RepoStatus::NOT_READY};
     std::unique_ptr<sdbus::IObject> dbus_object;
     std::string sender;
+    // repository key import confirmation
+    enum class KeyConfirmationStatus {PENDING, CONFIRMED, REJECTED};
+    std::mutex key_import_mutex;
+    std::condition_variable key_import_condition;
+    std::map<std::string, KeyConfirmationStatus> key_import_status{}; // map key_id: confirmation status
 };
 
 #endif
